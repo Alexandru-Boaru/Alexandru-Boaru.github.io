8c5efd460ec1a3a95fb29d524ba7994237843781
Alexandru Boaru <alex.boaru@gmail.com>
2023-01-11 14:18:11 +0200

    Upload files

---
+++ assets/js/data.js
@@ -0,0 +1,194 @@
+
+data = [
+    {
+        titleImage: "images/blobyscaledbanner1.png",
+        subImages : [
+            "images/blobyworld/bloby1.png",
+            "images/blobyworld/bloby2.png",
+            "images/blobyworld/bloby3.png",
+            "images/blobyworld/bloby4.png",
+            "images/blobyworld/bloby5.png",
+        ],
+        title: "Bloby's Journey",
+        date: "2019",
+        link : "https://ender-portal.itch.io/blobys-journey",
+        description:
+        `
+        Bloby's World is 2D platform game in which the player takes control of an alien blob. The goal of the game is to go through the levels and reach the finish line. During the levels, the players can also fight enemies and collect orbs in order to get a higher score. This project was created while learning the basics of game development with Unity, during my second bachelor year. It also opened new career posibilities to me, because it combines some of my main interests: programming, storytelling and visual arts.
+        <br>
+        Some of the gameplay features that I learnt how to implement are:
+        <ul>
+        <li>Basic platform motion commands: jump, double jump, crawl, glide</li>
+        <li>Basic combat commands: shield, punch</li>
+        <li>Two types of enemies with different behaviours and a multi-stage enemy boss</li>
+        <li>Level progression and level completition persistence between sessions</li>
+        <li>Different types of scene blocks with special behaviours: springs, spikes, jump-through platforms</li>
+        </ul>
+        
+        `
+    },
+
+    {
+        titleImage: "images/mythboundscaledbanner.png",
+        subImages : [
+            "images/mythbound/myth1.png",
+            "images/mythbound/myth2.png",
+            "images/mythbound/myth3.png",
+            "images/mythbound/myth4.png",
+            "images/mythbound/myth5.png",
+        ],
+        title: "Mythbound",
+        date: "2020",
+        link : "https://ender-portal.itch.io/mythbound",
+        description: 
+        `
+        A 3D puzzle adventure game made in Unity during the second semester of my third year as student. 
+        This game is a group project for the Software Engineering course. I was the main gameplay programmer of the team, 
+        as such my most important responsability was to manage the systems and components that the other team members developed. 
+        <br>
+        I was also tasked to work on gameplay features like: 
+        <ul>
+        <li><b>Player Controller</b>: the player can use the mouse to click on a scene surface or an object and the character uses the navmesh to find the shortest path toward destination.</li>
+        <li><b>Cameras</b>: the scene is populated by a collection of carefully positioned cameras that point toward scene segments. The current camera is switched once the character passes from one scene segment to another.</li>
+        <li><b>Scripted Events</b>: these events usually change the states of the items and the states of the mechanisms.</li>
+        <li><b>Puzzle Progression System</b>: I developed a system that allows the possibility of solving multiple puzzles in parallel.</li>
+        <li><b>User Interface</b>: inventory bar visuals and interaction.</li>
+        <ul/>
+        `
+        
+    },
+
+    {
+        titleImage: "images/snowfallscaledbanner.png",
+        subImages : [
+            "images/snowfall/snow1.png",
+            "images/snowfall/snow2.png",
+            "images/snowfall/snow3.png",
+            "images/snowfall/snow4.png",
+            "images/snowfall/snow5.png",
+        ],
+        title: "Snowfall",
+        date: "2020",
+        link : "https://ender-portal.itch.io/snowfall",
+        description: 
+        `
+        Snowfall is a 3D Adventure Game made in Unity in 48 hours, created by a two member team (Team Pixelpunk), for the 
+        <a href="https://itch.io/jam/clujotronic2020">Virtual Game Jam @ Clujotronic 2020</a>. 
+        I worked as a designer and a programmer and my tasks included:
+        <br>
+        <ul>
+        <li>Designing the scene setup</li>
+        <li>Setting up the physics system for the player character</li>
+        <li>Creating event scripts, systems and components</li>
+        <li>Creating an interaction system, used in order to be able to talk to NPCs and to move objects
+        </ul>
+        This is one of the more challenging projects I participated in due the 
+        competitivity and the time constraints. The game got 3rd place in the Popularity Category.
+        `
+        
+    },
+
+    {
+        titleImage: "images/debrisepochscaledbanner.png",
+        subImages : [
+            "images/debrisepoch/debris1.png",
+            "images/debrisepoch/debris2.png",
+            "images/debrisepoch/debris4.png",
+            "images/debrisepoch/debris5.png",
+            "images/debrisepoch/debris6.png",
+        ],
+        title: "Debris Epoch",
+        date: "2021",
+        link : "https://ender-portal.itch.io/debris-epoch",
+        description: 
+        `
+        This is a game that started as a project for the 3DUPB summer school project and continued as my bachelor diploma project. It is a 2D Turn-Based RPG in which the player controls the Traveler, a character that wanders in the wastelands of a post-apocaliptic world, 
+        helping small communities and hunting down the monsters that haunt the lands.<br>
+        The purpose of this project is to repopularise the Turn-Based RPGs in 
+        the video game industry. Following some interviews with possible clients and some market analysis, it 
+        has been chosen to implement a 2D retro style Turn-Based RPG, which incorporates classical features of this 
+        category, as well as some new functionality. 
+        Some of the core features implemented for this project are:
+        <ul>
+        <li>Player character movement controller: the game has a top-down perspective, so the player can move the character in any direction 
+        in a 2D plane</li>
+        <li>Player character stats: the player character can level up by gaining experience points through collecting resources and battling 
+        enemies. For each level-up, the player gains a stat point that can be used to increase one of the three character stats: HEALTH, 
+        POWER and SPEED</li>
+        <li>Battle system: the game uses a rpg turn based type of battle system. The player can control the actions of a single character. Some of the possible actions include: consuming an item, attacking one or more enemies at once and defending against enemy attacks</li>
+        <li>Inventory and resource system: the player can collect resources from containers around the game world and from defeated enemies</li>
+        <li>Weapons and abilities: the character uses three types of weapons, each one with different perks and abilities. These weapons 
+        can be upgraded using the collected resources</li>
+        <li>Quest system: the player receives different types of quests from the NPCs, such as Fetch Quests, in which the player needs to gather certain resources, Exploration Quests, in which the player needs to reach certain spots on in the game world, Battle Quests, in which the player needs to defeat certain enemies</li>
+        <li>Enemies and AI: the game world is filled with plenty of enemies that have different abilities and dificulties. When the player gets close enough to wandering enemies, they start chasing the player. Once the player is caught, a turn based battle starts</li>
+        <li>Game persistence and checkpoints: the player could save the current game progress at any available checkpoint in the world and resume the game from that place</li>
+        </ul>
+        Besides programming, I also created all the the visual art assets like the game world tiles, the characters and enemies sprites and animations, user interface and particle systems.
+        `
+        
+    },
+
+    {
+        titleImage: "images/robomazescaledbanner.png",
+        subImages : [
+            "images/robomaze/robo1.png",
+            "images/robomaze/robo2.png",
+            "images/robomaze/robo3.png",
+            "images/robomaze/robo4.png",
+            "images/robomaze/robo5.png",
+        ],
+        title: "ROBO-MAZE",
+        date: "2022",
+        link : "https://ender-portal.itch.io/robo-maze",
+        description: 
+        `
+        A FPS game created as part of a team project for a course. In this game the player explores mazes and collects blue boxes and fights 
+        enemy robots in order to get to the next level. The player can switch between four firearms with different pros and cons. 
+        Also, there are bonus levels in the shape of railroads with shooting targets, where the the player can easily gather more points and 
+        replenish resources like health points and bullets.
+        <br>
+        My tasks included:
+        <ul>
+        <li>Creating the movement and shooting systems for the characters</li>
+        <li>Creating scripts for the main gameplay loop and for the audio manager</li>
+        <li>Adding sound effects</li>
+        </ul>
+        `
+        
+    },
+
+    {
+        titleImage: "images/arealgamescaledbanner.png",
+        subImages : [
+            "images/arealgame/real1.png",
+            "images/arealgame/real2.png",
+            "images/arealgame/real3.png",
+            "images/arealgame/real4.png",
+            "images/arealgame/real5.png",
+        ],
+        title: "a REAL Game",
+        date: "2022",
+        link : "https://blsalin.itch.io/a-real-game",
+        description: 
+        `
+        a <i>REAL</i> game is project created by a team of three students in one week and submitted to the 
+        <a href="https://itch.io/jam/brackeys-7">Brackeys Game Jam 2022.1</a>, with theme being <b>"IT IS NOT REAL"</b>. We created a 3D platform game (with side view camera) in which the player guides a self-aware character.
+        There are also features that constantly remind the player that the world inside the game is a simulation:
+        <ul>
+        <li>Camera glitches</li>
+        <li>Unexpected events occurring in the level</li> 
+        <li>The character talks directly with the player</li>
+        <li>Computer terminal at the beginning of the game</li>
+        </ul>
+        
+        My tasks included: 
+        <ul>
+        <li>Creating the movement controller for player and enemy characters</li>
+        <li>Creating voice over and audio managers</li>
+        <li>Creating a simulated computer terminal</li>
+        <li>Writing and generating voice lines for the player character</li>
+        </ul>
+        `
+        
+    },
+]
---
+++ assets/js/page.switch.js
@@ -0,0 +1,39 @@
+
+window.addEventListener("load", loadPage)
+//document.addEventListener("click", loadPage)
+function loadPage()
+{
+    var queryString = location.search.substring(1);
+    var a = queryString.split("|");
+
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    if(page != "generic.html")
+        return;
+    if(a.length > 0){
+        var pageTitle = document.getElementById("pageTitle");
+        var pageDate = document.getElementById("pageDate");
+        var pageDescription = document.getElementById("pageDescription");
+        var pageLink = document.getElementById("pageLink");
+        var pageImage = document.getElementById("pageImage");
+        var pageSubimages = document.getElementById("pageSubimages");
+        
+        //console.log(data[Number(a[0])-1].title);
+        //console.log(pageTitle.innerHTML);
+
+        pageTitle.innerHTML = data[Number(a[0])-1].title;
+        pageDate.innerHTML = data[Number(a[0])-1].date;
+        pageDescription.innerHTML = data[Number(a[0])-1].description.replace(/(?:\r\n|\r|\n)/g, '');
+        pageLink.href = data[Number(a[0])-1].link;
+        
+        pageImage.src = data[Number(a[0])-1].titleImage;
+        pageSubimages.innerHTML = "";
+
+        for(let i = 0; i < data[Number(a[0])-1].subImages.length; i++)
+        {
+            //console.log(`<div class="col-2"><span class="image fit"><img class="myImg" src="${data[Number(a[0])-1].subImages[i]}" alt="${i}" onclick="displayModal(this)"></span></div>`);
+            pageSubimages.insertAdjacentHTML("beforeend", `<div class="col-2"><span class="image fit"><img class="myImg" src="${data[Number(a[0])-1].subImages[i]}" alt="${i}" onclick="displayModal(this)"></span></div>`)
+        }
+
+    }
+}
